2e89d084a481bda9ea9d87ae0464f9cc2b3f122a
suraj <surajmishra9318@gmail.com>
2022-12-24 12:18:27 +0530

    context file modifications

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react'
 // make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=8767e24b`
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
